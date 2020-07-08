b352486eaf2ff8beb7cc5d088406d4f43cc0ac55
akshay2739 <akshaypatel2739@gmail.com>
2020-07-08 18:12:28 +0530

    Sign up page styling done!

--- signup.php
+++ signup.php
@@ -1,24 +1,34 @@
 <?php
 
     require_once('include/database.php');
+    require_once('include/bootstrap.php')
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="height:100vh">
+    <div class=" h-100 d-flex justify-content-center align-items-center">
+        <form action="signup.php" method="post" class="bg-white p-5 shadow-lg form-group">
+            <input type="text" name="name" placeholder="Name" class="form-control mb-2" required>
+            
+            <input type="text" name="username" placeholder="username" class="form-control mb-2" required>
+            
+            <input name="password" id="password" type="password" placeholder="Password" class="form-control mb-2" onkeyup='check();' required>
 
-<form action="signup.php" method="post">
-    
-    name: <input type="text" name="name" required>
-    <br><br>
-    username: <input type="text" name="username" required>
-    <br><br>
-    password : <input name="password" id="password" type="password" onkeyup='check();' required>
+            <input type="password" name="confirm_password" id="confirm_password" class="form-control mb-2" placeholder="confirm password:" onkeyup='check();' required> 
+            <span id='message'></span>
+            
+            <input type="submit" name="signup" class="form-control text-white bg-success" value="Sign up">
+        </form>
+    </div>
+</body>
+</html>
 
-    <br><br>
-    confirm password:<input type="password" name="confirm_password" id="confirm_password"  onkeyup='check();' required> 
-    <span id='message'></span>
-<br>
-    
-    <input type="submit" name="signup" value="login">
-</form>
 
     <!-- if is_active = 1 then user is active else user is not active -->
 
