88b4cd4968e23f8f4ad56fef119a57ae0a52b2a8
akshay2739 <akshaypatel2739@gmail.com>
2020-07-09 12:21:57 +0530

    Category pade is styled!

--- categories.php
+++ categories.php
@@ -20,27 +20,60 @@ $res = $conn -> query($sql);
 </head>
 <body>
     <?php include_once('include/header.php') ?>
-    <form action="include/addCategorie.inc.php" method="POST">
-        <input type="text" name="categorie">
-        <input type="submit" name="add" value="Add Categorie">
-    </form>
+    
+    <div class="row p-lg-5">
+        <div class="col-lg-3 col-sm-12">
+            <div class="card">
+                <div class="card-header w-100">
+                    Add Category
+                </div>
+                <div class="card-body">
+                    <form action="include/addCategorie.inc.php" class="form-group" method="POST">
+                        <input type="text" class="form-control" name="categorie">
+                        <input type="submit" class="form-control bg-primary text-white mt-2" name="add" value="Add Categorie">
+                    </form>
+                </div>
+            </div>
+           
+           
+        </div>
+        <div class="col-lg-9 col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    All categories
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive-sm">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>    
+                                <th colspan=3>Actions</th>
+                            </tr>
+                            <?php while(($r = $res->fetch_assoc())){ ?>
+                                <tr>
+                                    <td><?php echo $r['category_id'] ?></td>
+                                    <td><?php echo $r['category_name']?></td>
+                                    <td> 
+                                        <a class="  btn btn-sm bg-warning text-white" href='edit_category.php?id=<?php echo $r['category_id']; ?>'> Edit </a>
+                                        <a class=" btn btn-sm bg-danger text-white" href='delete_category.php?id=<?php echo $r['category_id']; ?>'> Delete </a>
+                                        <a class="   btn btn-sm bg-success text-white" href='manageProducts.php?id=<?php echo $r['category_id']; ?>'>View Product</a>
+                                    </td>
+                                </tr>
+                            <?php } ?> 
+                        </table>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </div>
 
-    <table>
-        <tr>
-            <th>Category ID</th>
-            <th>Category Name</th>    
-            <th>Actions</th>
-        </tr>
-        <?php while(($r = $res->fetch_assoc())){ ?>
-            <tr>
-                <td><?php echo $r['category_id'] ?></td>
-                <td><?php echo $r['category_name']?></td>
-                <td> <a href='edit_category.php?id=<?php echo $r['category_id']; ?>'> Edit </a>
-                <a href='delete_category.php?id=<?php echo $r['category_id']; ?>'> Delete </a>
-                <td> <a href='manageProducts.php?id=<?php echo $r['category_id']; ?>'> View Products </a></td>
-            </tr>
-        <?php } ?> 
-    </table>
+
+    
+
+    
 </body>
 </html>
 
