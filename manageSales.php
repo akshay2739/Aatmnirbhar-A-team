ced256538678bf87f3624626d7e243b8088a00e6
desaiparv5 <desaiparv5@gmail.com>
2020-07-09 23:28:30 +0530

    sales page

--- manageSales.php
+++ manageSales.php
@@ -1,20 +1,7 @@
 <?php
-
     require_once('include/database.php');
     include('include/sessionCheck.php');
-
-?>
-
-
-<?php
-   
-
-    if(isset($_SESSION['id'])){
-        // echo '<script>alert("Status Updated");</script>';
-
-        unset($_SESSION['id']);
-    }
-
+    include('include/bootstrap.php');
 ?>
 
 <!-- HTML FOR DISPLAY OF SALES -->
@@ -32,38 +19,57 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h1 class="text-center text-uppercase">Manage sales</h1>
+                        <h1 class="text-center text-uppercase">Sales</h1>
                     </div>
                     <div class="table-responsive">
                         <table class="table table-center text-center">
                         
                             <tr>
                                 <th>No.</th>
+                                <th>Product</th>
+                                <th>Quantity</th>
+                                <th>Category</th>
                                 <th>TOTAL</th>
                                 <th>Date</th>
                                 <th>Status</th>
-                                <th>Update Status</th>
+                                <th>Change Status</th>
                             </tr>
                             <?php
-                                $index=0;
-                                $sql1="select * from sales order by id desc";
+                                $sql1="SELECT * FROM `sales` ORDER BY `id` DESC";
                                 $result1=$conn->query($sql1);
                                 while($r=$result1->fetch_assoc()){
-                                    $index=$index+1;
-                                ?>
-                                    <tr>
-                                        <td><?php echo $index; ?></td>
-                                        <td><?php echo $r['price']; ?></td>
-                                        <td><?php echo $r['date']; ?></td>
-                                        <td><?php echo $r['status']; ?></td>
-                                        <td><a href='update_status.php ? id=<?php echo $r['id']; ?>' class="btn btn-primary text-uppercase">change </a></td>
-
-                                    </tr>
-                                
-                                <?php
-                                    
-                                }
                             ?>
+                                <tr>
+                                    <td><?php echo $r['id']; ?></td>
+                                    <td><?php echo $r['p_name']; ?></td>
+                                    <td><?php echo $r['qty']; ?></td>
+                                    <td><?php echo $r['p_category']; ?></td>
+                                    <td><?php echo $r['price']; ?></td>
+                                    <td><?php echo $r['date']; ?></td>
+                                    <td><?php echo $r['status']; ?>
+                                    <td>
+                                        <?php
+                                            if($r['status']=='PENDING'){
+                                                echo("<a href='include/updateSalesStatus.php?id=".$r['id']."&action=PACKED' class='btn btn-primary text-uppercase btn-success'>
+                                                        PACKED<i class='fas fa-chevron-right'></i></a>");
+                                            }elseif($r['status']=='PACKED'){
+                                                echo("<a href='include/updateSalesStatus.php?id=".$r['id']."&action=PENDING' class='btn btn-primary text-uppercase btn-warning'>
+                                                        <i class='fas fa-chevron-left'></i>PENDING</a>");
+                                                echo("<a href='include/updateSalesStatus.php?id=".$r['id']."&action=SHIPPED' class='btn btn-primary text-uppercase  btn-success'>
+                                                        SHIPPED<i class='fas fa-chevron-right'></i></a>");
+                                            }elseif($r['status']=='SHIPPED'){
+                                                echo("<a href='include/updateSalesStatus.php?id=".$r['id']."&action=PACKED' class='btn btn-primary text-uppercase btn-warning'>
+                                                        <i class='fas fa-chevron-left'></i>PACKED</a>");
+                                                echo("<a href='include/updateSalesStatus.php?id=".$r['id']."&action=COMPLETED' class='btn btn-primary text-uppercase  btn-success'>
+                                                        COMPLETED<i class='fas fa-chevron-right'></i></a>");
+                                            }elseif($r['status']=='COMPLETED'){
+                                                echo("<a href='include/updateSalesStatus.php?id=".$r['id']."&action=SHIPPED' class='btn btn-primary text-uppercase btn-warning'>
+                                                        <i class='fas fa-chevron-left'></i>SHIPPED</a>");
+                                            }
+                                        ?>
+                                    </td>
+                                </tr>
+                                <?php } ?>
                         </table>
                     </div>
                 </div>
@@ -72,7 +78,3 @@
     </div>
 </body>
 </html>
-
-
-<!-- 
- -->
