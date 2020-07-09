b3ef15a6c3d3f858eb26b053c382b160982345c0
shiv2299 <shiv2299@gmail.com>
2020-07-09 19:05:30 +0530

    db

--- include/database.php
+++ include/database.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn=mysqli_connect("localhost","root","","warehouse");
+    $conn=mysqli_connect("localhost","root","","wms_db");
     if(mysqli_connect_error())
     {
         die('connect_error('.mysqli_connect_errno().')'.mysqli_connect_error());
